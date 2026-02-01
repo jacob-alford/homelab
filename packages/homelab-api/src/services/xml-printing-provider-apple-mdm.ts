46ae50ec3d61a905b9892fa519e353cbba825821
Jacob Alford <github.scouting378@passmail.net>
2026-01-31 18:29:59 -0700

    implement Apple XML printer

--- packages/homelab-api/src/services/xml-printing-provider-apple-mdm.ts
+++ packages/homelab-api/src/services/xml-printing-provider-apple-mdm.ts
@@ -1,25 +1,171 @@
-import { Context, Effect, Layer } from "effect"
-import { UuidService } from "./uuid-service.js"
-import { XmlPrintingService } from "./xml-printing-service.js"
+import { } from "@effect/typeclass"
+import * as A from "@effect/typeclass/data/Array"
+import * as R from "@effect/typeclass/data/Record"
+import * as Str from "@effect/typeclass/data/String"
+import * as Fld from "@effect/typeclass/Foldable"
+import * as Mn from "@effect/typeclass/Monoid"
+import * as Sg from "@effect/typeclass/Semigroup"
+import { Array, Context, Effect, Layer, pipe, Record } from "effect"
+import type { JSON } from "../schemas/JSON.js"
+import type { XmlPrintingServiceImpl } from "./xml-printing-service.js"
+import { XmlPrintingError, XmlPrintingService } from "./xml-printing-service.js"
 
 export const AppleMdmXmlPrintingConfigId = "AppleMdmXmlPrintingServiceConfig"
 
 export class AppleMdmXmlPrintingConfig extends Context.Tag(AppleMdmXmlPrintingConfigId)<
   AppleMdmXmlPrintingConfig,
-  {}
+  {
+    encoding?: string
+    newline?: string
+    indent?: string
+  }
 >() { }
 
 export const AppleMdmXmlPrintingLive = Layer.effect(
   XmlPrintingService,
   Effect.gen(function*() {
     const config = yield* AppleMdmXmlPrintingConfig
-    const UUID = yield* UuidService
 
-    return {
-      printXml(json) {
-        // TODO
-        return Effect.die(new Error("Not implemented"))
-      },
-    }
+    return new AppleMdmXmlPrintingImpl(config)
   }),
 )
+
+class AppleMdmXmlPrintingImpl implements XmlPrintingServiceImpl {
+  private encoding: string
+  private newline: string
+  private indent: string
+
+  constructor(
+    {
+      encoding = "UTF-8",
+      indent = "  ",
+      newline = "\n",
+    }: typeof AppleMdmXmlPrintingConfig.Service,
+  ) {
+    this.encoding = encoding
+    this.newline = newline
+    this.indent = indent
+  }
+
+  printXml(json: Record<string, JSON>): Effect.Effect<string, XmlPrintingError> {
+    return pipe(
+      this.encodeContent(json),
+      Effect.map(
+        (content) =>
+          Sg.intercalate(this.newline)(Str.Semigroup).combineMany(
+            this.xmlLeader,
+            [
+              this.doctype,
+              this.b`${"plist"}${{ version: "1.0" }}${content}${0}`,
+            ],
+          ),
+      ),
+      Effect.map(
+        (result) => result.replaceAll("\n", this.newline),
+      ),
+    )
+  }
+
+  private get xmlLeader() {
+    return `<?xml version="1.0" encoding="${this.encoding}"?>`
+  }
+
+  private get doctype() {
+    return `<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">`
+  }
+
+  private b(
+    [s]: TemplateStringsArray,
+    node: string,
+    attributes: Record<string, string> = {},
+    content: string,
+    indent: number = 0,
+  ): string {
+    const attributesStr = pipe(
+      attributes,
+      Record.collect((k, v) => `${k}=${this.escape(v)}`),
+      Array.join(" "),
+    )
+
+    return `${s !== node ? s : ""}${this.indent.repeat(indent)}<${node}${attributesStr === "" ? "" : attributesStr
+      }>${content}</${node}>`
+  }
+
+  private escape(str: string): string {
+    return str
+      .replace(/&/g, "&amp;")
+      .replace(/</g, "&lt;")
+      .replace(/>/g, "&gt;")
+      .replace(/"/g, "&quot;")
+      .replace(/'/g, "&apos;")
+  }
+
+  private encodeContent(json: JSON, depthLevel = 0): Effect.Effect<string, XmlPrintingError> {
+    if (json === null) {
+      return Effect.fail(
+        new XmlPrintingError({
+          error: "Null values are not allowed",
+        }),
+      )
+    }
+
+    if (typeof json === "string") {
+      return Effect.succeed(this.b`\n${"string"}${{}}${this.escape(json)}${depthLevel}`)
+    }
+
+    if (typeof json === "number") {
+      if (Number.isNaN(json) || !Number.isSafeInteger(json)) {
+        return Effect.fail(
+          new XmlPrintingError({
+            error: `Receieved invalid number: ${json}`,
+          }),
+        )
+      } else {
+        return Effect.succeed(this.b`\n${"integer"}${{}}${String(json)}${depthLevel}`)
+      }
+    }
+
+    if (typeof json === "boolean") {
+      return Effect.succeed(`\n${this.indent.repeat(depthLevel)}<${json}/>`)
+    }
+
+    if (this.isObject(json)) {
+      return Effect.reduce(
+        Record.toEntries(json),
+        "",
+        (z, [k, v]) =>
+          this.encodeContent(v, depthLevel + 1).pipe(
+            Effect.map(
+              (children) =>
+                z + this.b`\n${"dict"}${{}}${this.b`\n${"key"}${{}}${k}${depthLevel + 1}` + children}${depthLevel}`,
+            ),
+          ),
+      )
+    }
+
+    if (this.isArray(json)) {
+      return Effect.reduce(
+        json,
+        "",
+        (z, v) =>
+          this.encodeContent(v, depthLevel + 1).pipe(
+            Effect.map(
+              (children) => z + this.b`\n${"array"}${{}}${children}${depthLevel}`,
+            ),
+          ),
+      )
+    }
+
+    return Effect.die(
+      new Error(`Unexpected type passed to AppleMdmXmlPrintingServiceImpl#printXml: ${String(json)}`),
+    )
+  }
+
+  private isObject(json: JSON): json is Record<string, JSON> {
+    return json !== null && typeof json === "object" && !Array.isArray(json)
+  }
+
+  private isArray(json: JSON): json is Array<JSON> {
+    return Array.isArray(json)
+  }
+}
--- packages/homelab-api/src/services/xml-printing-service.ts
+++ packages/homelab-api/src/services/xml-printing-service.ts
@@ -5,7 +5,9 @@ import type { JSON } from "../schemas/JSON.js"
 
 export const XmlPrintingServiceId = "homelab-api/services/xml-printing-service/XmlPrintingService"
 
-export class XmlPrintingError extends Data.TaggedError("XmlPrintingError")<{}> { }
+export class XmlPrintingError extends Data.TaggedError("XmlPrintingError")<{
+  readonly error: unknown
+}> { }
 
 export interface XmlPrintingServiceImpl {
   printXml(json: Record<string, JSON>): Effect.Effect<string, XmlPrintingError>
