baa70e5f0473a3aef83af609de533c40bbc01697
Jacob Alford <github.scouting378@passmail.net>
2026-03-22 12:49:44 -0600

    stand up base astro/solidjs template

---
+++ packages/homelab-frontend/src/components/Landing.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@kobalte/core/button"
+import "./Landing.css"
+
+export function Landing() {
+  return (
+    <div class="landing">
+      <Button class="landing__button">Login</Button>
+    </div>
+  )
+}
