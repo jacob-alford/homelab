6effc27dbe238b1ad8fdba33cd4e0d4b1ecf483d
Jacob Alford <github.scouting378@passmail.net>
2026-03-22 14:29:24 -0600

    add whole-repo formatting to pre-commit

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,9 +1,12 @@
 export default {
+  "*": [
+    "dprint fmt --incremental",
+  ],
   "*.{ts,tsx,js,jsx,mjs}": [
     "eslint --fix",
     "yarn test:staged",
   ],
-  "*": [
+  "*.{ts,tsx}": [
     () => "yarn lint",
     () => "yarn typecheck",
   ],
