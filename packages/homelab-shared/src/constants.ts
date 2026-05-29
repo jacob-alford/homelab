e3e5507b773d4f5fc44b62dbf54ad5a44f0bd713
Jacob Alford <github.scouting378@passmail.net>
2026-05-29 13:28:02 -0600

    fix roles claim name requirement

--- packages/homelab-shared/src/constants.ts
+++ packages/homelab-shared/src/constants.ts
@@ -1,6 +1,6 @@
 export const BASE_KEY = "plato-splunk.services.homelab-api"
 
-export const JWT_ROLES_KEY = `${BASE_KEY}.jwt.roles`
+export const JWT_ROLES_KEY = "roles"
 
 export const JWT_HOMELAB_API_AUD = `${BASE_KEY}.jwt.audience`
 
